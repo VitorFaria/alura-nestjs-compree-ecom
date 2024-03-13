99688f57a60018ae983c3629df9180412a6b2648
Vitor Faria <vitor.faria@ignicaodigital.com.br>
2024-03-12 21:59:43 -0300

    Adiciona dto para mapear status da order a ser atualizado

--- src/order/dto/update-order.dto.ts
+++ src/order/dto/update-order.dto.ts
@@ -1,4 +1,7 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateOrderDto } from './create-order.dto';
+import { IsEnum } from 'class-validator';
+import { OrderStatus } from '../enum/OrderStatus.enum';
 
-export class UpdateOrderDto extends PartialType(CreateOrderDto) {}
+export class UpdateOrderDto {
+  @IsEnum(OrderStatus)
+  orderStatus: OrderStatus;
+}
