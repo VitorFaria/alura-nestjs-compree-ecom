5c83717ed142b80dc5c11b5aba55013e4f515dd3
Vitor Faria <vitor.faria@ignicaodigital.com.br>
2024-03-13 03:38:07 -0300

    Implementa try catch block nos métodos que estão sendo utilizados em product

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -14,45 +14,60 @@ export class ProductService {
   ) {}
 
   async createProduct(createProductDTO: CreateProductDTO) {
-    const productEntity = new ProductEntity();
+    try {
+      const productEntity = new ProductEntity();
+  
+      productEntity.name = createProductDTO.name;
+      productEntity.value = createProductDTO.value;
+      productEntity.amountAvailable = createProductDTO.amountAvailable;
+      productEntity.description = createProductDTO.description;
+      productEntity.category = createProductDTO.category;
+      productEntity.features = createProductDTO.features;
+      productEntity.images = createProductDTO.images;
+  
+      const product = await this.productRepository.save(productEntity); 
 
-    productEntity.name = createProductDTO.name;
-    productEntity.value = createProductDTO.value;
-    productEntity.amountAvailable = createProductDTO.amountAvailable;
-    productEntity.description = createProductDTO.description;
-    productEntity.category = createProductDTO.category;
-    productEntity.features = createProductDTO.features;
-    productEntity.images = createProductDTO.images;
-
-    return this.productRepository.save(productEntity);
+      return product;
+    } catch (err) {
+      throw err;
+    }
   }
 
   async listProducts() {
-    const products = await this.productRepository.find({
-      relations: {
-        images: true,
-        features: true,
-      },
-    });
-    const mappedProducts = products.map(
-      (product) =>
-        new ListProductDTO(
-          product.id,
-          product.name,
-          product.features,
-          product.images,
-        ),
-    );
-    return mappedProducts;
+    try {
+      const products = await this.productRepository.find({
+        relations: {
+          images: true,
+          features: true,
+        },
+      });
+      const mappedProducts = products.map(
+        (product) =>
+          new ListProductDTO(
+            product.id,
+            product.name,
+            product.features,
+            product.images,
+          ),
+      );
+      return mappedProducts;
+
+    } catch (err) {
+      throw err;
+    }
   }
 
   async updateProduct(id: string, updateProductDTO: UpdateProductDTO) {
-    const product = await this.productRepository.findOneBy({ id });
-
-    if (!product) throw new NotFoundException("Product not found");
-
-    Object.assign(product, updateProductDTO);
-    return this.productRepository.save(product);
+    try {
+      const product = await this.productRepository.findOneBy({ id });
+  
+      if (!product) throw new NotFoundException("Product not found");
+  
+      Object.assign(product, updateProductDTO);
+      return this.productRepository.save(product);
+    } catch(err) {
+      throw err;
+    }
   }
 
   async removeProduct(id: string) {
