0c14b17e2547b3a30d07d04d156e2dad929feb1a
Vitor Faria <vitor.faria@ignicaodigital.com.br>
2024-03-20 03:11:47 -0300

    Muda caminho do exceptionFilter

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { UserModule } from './modules/user/user.module';
 import { PostgresConfigService } from './config/postgres.config.service';
 import { OrderModule } from './modules/order/order.module';
 import { APP_FILTER } from '@nestjs/core';
-import { ExceptionFilterGlobal } from './filters/exception-filter-global';
+import { ExceptionFilterGlobal } from './resources/filters/exception-filter-global';
 
 @Module({
   imports: [
