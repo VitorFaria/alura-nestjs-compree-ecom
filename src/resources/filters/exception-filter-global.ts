d5bea9cba5743882339ad97908538de7593dbe06
Vitor Faria <vitor.faria@ignicaodigital.com.br>
2024-04-07 16:00:29 -0300

    Ajusta exception filter global para exibir log informativo no console e salvar log de erro em arquivo

--- src/resources/filters/exception-filter-global.ts
+++ src/resources/filters/exception-filter-global.ts
@@ -1,17 +1,29 @@
-import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from "@nestjs/common";
+import { ArgumentsHost, Catch, ConsoleLogger, ExceptionFilter, HttpException, HttpStatus } from "@nestjs/common";
 import { HttpAdapterHost } from "@nestjs/core";
+import { CustomLogger } from "src/modules/customLogger/logger.service";
+import { UserRequest } from '../../modules/auth/auth.guard';
 
 @Catch()
 export class ExceptionFilterGlobal implements ExceptionFilter {
-  constructor(private adapterHost: HttpAdapterHost) {}
+  constructor(
+    private adapterHost: HttpAdapterHost
+  ) {}
+
+  private readonly logger = new CustomLogger(ExceptionFilterGlobal.name)
 
   catch(exception: unknown, host: ArgumentsHost) {
-
     const { httpAdapter } = this.adapterHost;
 
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
-    const request = ctx.getRequest<Request>();
+    const request = ctx.getRequest<Request | UserRequest>();
+
+    const endpoint = httpAdapter.getRequestUrl(request);
+    const method = httpAdapter.getRequestMethod(request);
+    
+    if ('user' in request) {
+      this.logger.log(`Endpoint: ${endpoint} | Action: ${method} | acessado pelo usuário ${request.user.sub}`)
+    }
 
     const { status, body} = exception instanceof HttpException
       ? {
@@ -23,9 +35,14 @@ export class ExceptionFilterGlobal implements ExceptionFilter {
         body: {
           statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
           timestamp: new Date().toISOString(),
-          url: httpAdapter.getRequestUrl(request)
+          method: method,
+          url: endpoint
         }
       }
+    
+    if (status === HttpStatus.INTERNAL_SERVER_ERROR) {
+      this.logger.error(exception, ExceptionFilterGlobal.name)
+    }
 
     httpAdapter.reply(response, body, status)
   }
